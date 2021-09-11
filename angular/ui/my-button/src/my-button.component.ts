a873be410074c61519d8b8a4aed8e04163a5369e
Romeo Bellon <romeo.bellon@me.com>
2021-09-11 22:34:38 +0200

    chore: init repo

---
+++ angular/ui/my-button/src/my-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-button',
+  template: `<p>my-button works!</p>`,
+  styles: [
+  ]
+})
+export class MyButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
